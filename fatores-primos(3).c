db55273fd6b01d2f01311fae9d1e55628803ccae
Gabriel Ramos <ramos.gabrielsilva23@gmail.com>
2022-06-28 19:35:41 -0300

    Add files via upload

---
+++ fatores-primos(3).c
@@ -0,0 +1,18 @@
+/*
+#include<stdio.h>
+
+int main(){
+
+    int i;
+    long long num = 600851475143;
+
+    for(i=2; i<num; i){
+        if(num%i == 0){
+            num = num/i;
+            continue;
+        }
+        i++;
+    }
+    printf("%d", num);
+}
+*/
